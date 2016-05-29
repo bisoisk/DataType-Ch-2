c5dccae6dd4118974aa46aee4bf2ca25c6c32479
bisoisk <bisoisusant@gmail.com>
2016-05-29 15:16:45 +0530

    DataTypes- in C#-Ch2-Exp
    DataTypes Solution file

---
+++ PrimitiveDataTypes/PrimitiveDataTypes/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PrimitiveDataTypes
+{
+    public sealed partial class MainWindow : Page
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void typeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem selectedType = (type.SelectedItem as ListBoxItem);
+            switch (selectedType.Content.ToString())
+            {
+                case "int":
+                    showIntValue();
+                    break;
+                case "long":
+                    showLongValue();
+                    break;
+                case "float":
+                    showFloatValue();
+                    break;
+                case "double":
+                    showDoubleValue();
+                    break;
+                case "decimal":
+                    showDecimalValue();
+                    break;
+                case "string":
+                    showStringValue();
+                    break;
+                case "char":
+                    showCharValue();
+                    break;
+                case "bool":
+                    showBoolValue();
+                    break;
+            }
+        }
+
+        private void showIntValue()
+        {
+           //Done by Susant for Practice
+            int intvar;
+            intvar = 1001;
+            value.Text = intvar.ToString();
+
+
+           //Done by Susant for Practice
+            //value.Text = "to do";
+        }
+
+        private void showLongValue()
+        {
+            long longVar;
+            longVar = 42L;
+            value.Text = longVar.ToString();
+        }
+
+        private void showFloatValue()
+        {
+            float floatVar;
+            floatVar = 0.42F;
+            value.Text = floatVar.ToString();
+        }
+
+        private void showDoubleValue()
+        {
+            /*added by susant for practice.... */
+            //value.Text = "to do";
+            double Doublevalue;
+           // Doublevalue = 11^1000003;
+
+            //here 7.6e+14 means 7.6 followed ny 14no of zeroes...means 7.6*100000000000000
+            Doublevalue = 7.6e+14;
+            value.Text = Doublevalue.ToString();
+
+        }
+
+        private void showDecimalValue()
+        {
+            decimal decimalVar;
+            decimalVar = 0.42M;
+            value.Text = decimalVar.ToString();
+        }
+
+        private void showStringValue()
+        {
+            string stringVar;
+            stringVar = "forty two";
+            value.Text = stringVar; // ToString not needed
+        }
+
+        private void showCharValue()
+        {
+            char charVar;
+            charVar = 'x';
+            value.Text = charVar.ToString();
+        }
+
+        private void showBoolValue()
+        {
+           /*Added by Susant for Bool value Test... */
+            bool boolvar;
+            boolvar = true;
+            value.Text = boolvar.ToString();
+        }
+    }
+}
